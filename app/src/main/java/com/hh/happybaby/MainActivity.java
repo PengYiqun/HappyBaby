e22126a80b44f76f2380335c8bcad7d10b8590e3
hhyifeng <971907847@qq.com>
2016-06-28 13:59:27 +0800

    add material design aar

--- app/src/main/java/com/hh/happybaby/MainActivity.java
+++ app/src/main/java/com/hh/happybaby/MainActivity.java
@@ -3,7 +3,7 @@ package com.hh.happybaby;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.lid.lib.LabelView;
-
+import com.gc.materialdesign.views.ButtonFlat;
 public class MainActivity extends AppCompatActivity {
 
     @Override
