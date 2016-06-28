2b6f4f9891aed58d12fd6c653943626d61adb981
PengYiqun <pengyiqun1995@qq.com>
2016-06-28 17:26:59 +0800

    tt

--- app/src/main/java/com/hh/happybaby/MainActivity.java
+++ app/src/main/java/com/hh/happybaby/MainActivity.java
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.lid.lib.LabelView;
 import com.gc.materialdesign.views.ButtonFlat;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/hh/happybaby/UI/FooterController.java
@@ -0,0 +1,12 @@
+package com.hh.happybaby.UI;
+
+/**
+ * Created by PengYiqun on 2016/6/28.
+ * 处理页面底部的四个按钮的点击事件
+ *
+ */
+public class FooterController {
+    private
+
+
+}
